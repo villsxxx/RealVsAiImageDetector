b6e690639b61f076b32b16d16e15a3bf0f73802a
shredder-vrn <shcherbatovr3ds@gmail.com>
2026-02-19 22:34:33 +0300

    Create Baseline Classes

---
+++ Model/ResNet.py
@@ -0,0 +1,11 @@
+import torch
+
+class ResBlock():
+    def __init__(self):
+        pass
+
+class CustomResNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+    def forward(self, x):
+        return x
